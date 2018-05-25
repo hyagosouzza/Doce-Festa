e1684675914faea24908b9444174f5481aac0c52
Hyago Souza <hyagosouzza@hotmail.com>
2018-05-25 11:58:03 -0300

    aplicando diretivas e unico app.js

--- public/app.js
+++ public/app.js
@@ -18,7 +18,73 @@
 
             $scope.maskPhone = "";
 
+            var db = firebase.firestore();
+            $scope.produtos = [];
+            $scope.load = false;
+            db.collection("Produtos")
+                .onSnapshot(function (querySnapshot) {
+                    querySnapshot.forEach(function (doc) {
+                        const dataProduto = doc.data()
+                        dataProduto.id = doc.id;
+                        $scope.produtos.push(dataProduto);
+                    });
+                    $scope.load = true;
+                });
+
+            $scope.marcas = [];
+            db.collection("Marcas")
+                .onSnapshot(function (querySnapshot) {
+                    querySnapshot.forEach(function (doc) {
+                        const dataMarca = doc.data()
+                        dataMarca.id = doc.id;
+                        $scope.marcas.push(dataMarca);
+                    });
+                });
+
+            $scope.temas = [];
+            db.collection("Temas")
+                .onSnapshot(function (querySnapshot) {
+                    querySnapshot.forEach(function (doc) {
+                        const dataTema = doc.data()
+                        dataTema.id = doc.id;
+                        $scope.temas.push(dataTema);
+                    });
+                });
+
+            $scope.filtrarTag = function (tag) {
+                $scope.tag = tag;
+            }
+
+
+            $scope.filtrarMarca = function (marca) {
+                $scope.marca = marca;
+            }
+
+            $scope.filtrarProd = function (prod) {
+                $scope.prod = prod;
+            }
+
+            var refMarca = firebase.database().ref();
+
+            var objMarca = $firebaseArray(refMarca);
+
         }
     ]);
+
+    app.directive('menuBar', function () {
+        return {
+            templateUrl: './diretivas/menubar.html'
+        };
+    });
+    app.directive('emailModal', function () {
+        return {
+            templateUrl: './diretivas/emailmodal.html'
+        };
+    });
+    app.directive('rodape', function () {
+        return {
+            templateUrl: './diretivas/rodape.html'
+        };
+    });
 
 }());
--- public/script.js
+++ public/script.js
@@ -1,7 +1,22 @@
 $(document).ready(function () {
+    $(document).mousemove(function () {
+        if (!justHidden) {
+            justHidden = false;
+            clearTimeout(j);
+            $('.btn-to-top').removeClass('hidden');
+            j = setTimeout('hide();', 1000);
+        }
+    });
+    $(document).on('click', '[data-toggle="lightbox"]', function (event) {
+        event.preventDefault();
+        $(this).ekkoLightbox();
+    });
     $('.list-group-item').click(function () {
         $('.list-group-item.active').removeClass('active');
         $(this).addClass('active');
+        $('html, body').animate({
+            scrollTop: $("#vertical").offset().top
+        }, 700);
     })
     $(function () {
         $('[data-toggle="popover"]').popover({ html: true });
@@ -18,7 +33,7 @@ $(document).ready(function () {
             $('.main').addClass('animated fadeInDown');
             $('nav.container.navbar.navbar-expand-lg.navbar-light').css("opacity", "0.9");
 
-        } else if($(this).scrollTop() <= 100){
+        } else if ($(this).scrollTop() <= 100) {
             $('.main').removeClass('sticky-top');
             $('.navbar-brand').removeClass('img-fixed');
             $('.navbar-brand').addClass('img-normal');
